931269ed69776c9cef34917f0f7be5f7c1511907
Evgeniy Kotovich <sxekot@gmail.com>
2022-04-06 00:27:19 +0300

    Initial Commit

---
+++ XylophoneNoStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XylophoneNoStoryboard
+//
+//  Created by Eugene Kotovich on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
